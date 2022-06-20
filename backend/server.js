45653a30dfea4bc6f607dffed95efb42944877d7
omar199523 <model19952323@gmail.com>
2022-06-20 18:47:10 +0300

    add main get

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ dbConnect();
 app.use(express.json());
 app.use(express.urlencoded({extended :false}));
 app.use(errorHandler);
-
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
 app.use('/api/goals',require('./routes/goalsRoutes'))
 
 app.use('/api/user',require('./routes/userRoutes'))
